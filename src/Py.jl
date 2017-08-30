aa86ff1b585ea64debb0c5044d1781d77ed92d88
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2017-08-30 17:01:02 -0600

    Update Py.jl

--- src/Py.jl
+++ src/Py.jl
@@ -1,5 +1,34 @@
 module Py
 
-# package code goes here
+using PyCall
+
+function traverse!{T}(arg::T)::T
+    return arg
+end
+
+function traverse!(expr::Expr)
+
+    if expr.head == :ref
+        expr.head = :call
+        unshift!(expr.args, :get)
+    end
+
+    for (i, arg) in enumerate(expr.args)
+        traverse!(expr.args[i])
+    end
+
+    if expr.head == :.
+        expr.head = :ref
+    end
+
+    return expr
+end
+
+macro Py(expr::Expr)
+
+    traverse!(expr)
+
+    return expr
+end
 
 end # module
