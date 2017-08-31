c2d88b0cd98a896d639aea4d2783572b4ed08d24
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2017-08-30 18:38:08 -0600

    Update test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,7 @@ using Py
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+@test @py(:(df)) == :(df)
+@test @py(:(df.loc)) == :(df[:loc])
+@test @py(:(df.loc[:])) == :(get(df[:loc], (pybuiltin("slice"))(nothing, nothing, nothing)))
+@test @py(:(df.loc[0:5, "salary1":"salary2"].plot.bar(linestyle="--"))) == :((((get(df[:loc], (pybuiltin("slice"))(0, 5, nothing), (pybuiltin("slice"))("salary1", "salary2", nothing)))[:plot])[:bar])(linestyle="--"))
